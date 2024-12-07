0418c0dc9752b99b4640a9703ede5a3596673cb6
Hitana <mcr.hitana@gmail.com>
2024-12-07 13:38:42 -0300

    Desenvolvimento JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcular (num1, num2) {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+    console.log(`${num1} x ${num2} = ${num1 * num2}`);
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+
+// Teste com os números 4 e 5
+calcular(4, 5);
